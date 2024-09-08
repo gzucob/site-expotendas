73e0529ab99a12bd41721d7b9d2dfe2d5a3e20a0
Gabriel Zuco <brezoliin@gmail.com>
2024-09-08 13:24:49 -0300

    Adicionado DOM para galeria e controles via teclado

--- main.js
+++ main.js
@@ -82,94 +82,125 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(animateSlider, 10); // Velocidade controlada pela frequência do intervalo
 });
 
-// Config Galeria Imagens Desktop/Mobile
-const images = document.querySelectorAll('.galeria-img');
-let currentIndex = 0;
-
-images.forEach((img, index) => {
-	img.addEventListener('click', () => {
-		openOverlayGaleria(index);
-	});
-});
-
-function openOverlayGaleria(index) {
-	currentIndex = index;
-	const overlay = document.getElementById('overlay-galeria');
-	const overlayImg = document.getElementById('overlay-galeria-img');
-	overlayImg.src = images[index].src;
-	overlay.classList.add('show');
-	document.body.style.overflow = 'hidden'; // Bloqueia a rolagem da página
-	addGestureControls();
-}
-
-function closeOverlayGaleria() {
-	const overlay = document.getElementById('overlay-galeria');
-	overlay.classList.remove('show');
-	document.body.style.overflow = 'auto'; // Desbloqueia a rolagem da página
-	removeGestureControls();
-}
-
-function showPrev() {
-	currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
-	const overlayImg = document.getElementById('overlay-galeria-img');
-	overlayImg.src = images[currentIndex].src;
-}
-
-function showNext() {
-	currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
-	const overlayImg = document.getElementById('overlay-galeria-img');
-	overlayImg.src = images[currentIndex].src;
-}
-
-function addGestureControls() {
-	const overlay = document.getElementById('overlay-galeria');
-	overlay.addEventListener('touchstart', handleTouchStart, false);
-	overlay.addEventListener('touchmove', handleTouchMove, false);
-}
-
-function removeGestureControls() {
-	const overlay = document.getElementById('overlay-galeria');
-	overlay.removeEventListener('touchstart', handleTouchStart, false);
-	overlay.removeEventListener('touchmove', handleTouchMove, false);
-}
-
-let xDown = null;
-let yDown = null;
-
-function getTouches(evt) {
-	return evt.touches || evt.originalEvent.touches;
-}
-
-function handleTouchStart(evt) {
-	const firstTouch = getTouches(evt)[0];
-	xDown = firstTouch.clientX;
-	yDown = firstTouch.clientY;
-}
-
-function handleTouchMove(evt) {
-	if (!xDown || !yDown) {
-		return;
-	}
-
-	let xUp = evt.touches[0].clientX;
-	let yUp = evt.touches[0].clientY;
-
-	let xDiff = xDown - xUp;
-	let yDiff = yDown - yUp;
-
-	if (Math.abs(xDiff) > Math.abs(yDiff)) {
-		if (xDiff > 0) {
-			showNext();
-		} else {
-			showPrev();
-		}
-	}
-
-	xDown = null;
-	yDown = null;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Config Galeria Imagens Desktop/Mobile
+    const images = document.querySelectorAll('.galeria-img');
+    let currentIndex = 0;
+
+    images.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            openOverlayGaleria(index);
+        });
+    });
+
+    function openOverlayGaleria(index) {
+        currentIndex = index;
+        const overlay = document.getElementById('overlay-galeria');
+        const overlayImg = document.getElementById('overlay-galeria-img');
+        overlayImg.src = images[index].src;
+        overlay.classList.add('show');
+        document.body.style.overflow = 'hidden'; // Bloqueia a rolagem da página
+        addGestureControls(); // Adiciona controles de toque
+        addKeyboardControls(); // Adiciona controles do teclado
+    }
+
+    function closeOverlayGaleria() {
+        const overlay = document.getElementById('overlay-galeria');
+        overlay.classList.remove('show');
+        document.body.style.overflow = 'auto'; // Desbloqueia a rolagem da página
+        removeGestureControls(); // Remove controles de toque
+        removeKeyboardControls(); // Remove controles do teclado
+    }
+
+    function showPrev() {
+        currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+        const overlayImg = document.getElementById('overlay-galeria-img');
+        overlayImg.src = images[currentIndex].src;
+    }
+
+    function showNext() {
+        currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+        const overlayImg = document.getElementById('overlay-galeria-img');
+        overlayImg.src = images[currentIndex].src;
+    }
+
+    // Adiciona controles de teclado
+    function addKeyboardControls() {
+        document.addEventListener('keydown', handleKeyDown);
+    }
+
+    // Remove controles de teclado
+    function removeKeyboardControls() {
+        document.removeEventListener('keydown', handleKeyDown);
+    }
+
+    // Função que lida com as teclas pressionadas
+    function handleKeyDown(event) {
+        if (event.key === 'ArrowLeft') {
+            showPrev();
+        } else if (event.key === 'ArrowRight') {
+            showNext();
+        } else if (event.key === 'Escape') {
+            closeOverlayGaleria();
+        }
+    }
+
+    // Controle por toque (gesto)
+    function addGestureControls() {
+        const overlay = document.getElementById('overlay-galeria');
+        overlay.addEventListener('touchstart', handleTouchStart, false);
+        overlay.addEventListener('touchmove', handleTouchMove, false);
+    }
+
+    function removeGestureControls() {
+        const overlay = document.getElementById('overlay-galeria');
+        overlay.removeEventListener('touchstart', handleTouchStart, false);
+        overlay.removeEventListener('touchmove', handleTouchMove, false);
+    }
+
+    let xDown = null;
+    let yDown = null;
+
+    function getTouches(evt) {
+        return evt.touches || evt.originalEvent.touches;
+    }
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    }
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        let xUp = evt.touches[0].clientX;
+        let yUp = evt.touches[0].clientY;
+
+        let xDiff = xDown - xUp;
+        let yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff > 0) {
+                showNext(); // Gesto para a esquerda
+            } else {
+                showPrev(); // Gesto para a direita
+            }
+        }
+
+        // Resetando valores
+        xDown = null;
+        yDown = null;
+    }
+
+    // Vinculando eventos aos botões após o carregamento do DOM
+    document.querySelector('.prev-btn').addEventListener('click', showPrev);
+    document.querySelector('.next-btn').addEventListener('click', showNext);
+    document.querySelector('.close-btn').addEventListener('click', closeOverlayGaleria);
+
+    // Botão para abrir/fechar galeria
     var btnGaleria = document.getElementById('btnGaleria');
     var collapseElement = document.getElementById('collapseExample');
 
