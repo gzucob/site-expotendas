5cbadb2cc197ace722e202e35e51c5cbd0590d77
gzucob <brezoliin@gmail.com>
2025-11-28 11:17:01 -0300

    Adicionado error404.html ao dist para corrgir bug de loop infinito quando digitado alguma URL inexistente

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        error404: resolve(__dirname, 'error404.html'),
+      },
+    },
+  },
+});
